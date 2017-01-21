a47843e77d895ebd50b25244091170e6b027cacb
Apollo Tang <tang.apollo@gmail.com>
2017-01-21 15:59:28 -0500

    added 02-webpack-dev-server

---
+++ 02-00-webpack-dev-server/webpack.config.js
@@ -0,0 +1,34 @@
+const pathResolve = require('path').resolve;
+
+// webpack-dev-server configuration
+const config = {
+    devServer: {
+        contentBase: pathResolve('public') // [1]
+    },
+    context: pathResolve('src'),           // [2]
+    entry: './main.js',                    // [3]
+    output: {
+        publicPath: 'scripts/',            // [4]
+        filename: 'bundle.js',             // [5]
+    },
+};
+
+module.exports = config;
+
+//
+// [1] • Root of the webpack's http dev-server.
+//     • webpack-dev-server deliver index.html file from this location.
+//     • Must be absolute path.
+//
+// [2] • Webpack look for its input from this location.
+//     • Must be absolute path.
+//
+// [3] • Name of Webpack's input file.
+//
+// [4] • Webpack-dev-server serves Webpack's output via this path.
+//     • Not absolute path, relative to devServer.contentBase.
+//     • Output are served from memory, not from file.system.
+//
+// [5] • Name of the output.
+//
+
