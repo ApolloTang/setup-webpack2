4a3c74ae13fb4fe8a4f55604c5371c525c20e2eb
Apollo Tang <tang.apollo@gmail.com>
2017-01-27 10:01:27 -0500

    add lesson 4 clean up (wip)

---
+++ 04-00-clean-up/webpack.config.js
@@ -0,0 +1,35 @@
+const pathResolve = require('path').resolve;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin'); // [wp|4]
+
+
+const webpackDevServer_host = '0.0.0.0';
+const webpackDevServer_port = 8080;
+
+const absolutePath_sourceFolder = pathResolve('src');
+const absolutePath_buildFolder = pathResolve('dist');
+
+
+const config = {
+    devServer: {
+        host: webpackDevServer_host,   // [wp|4*]
+        port: webpackDevServer_port    // [wp|4*]
+    },
+    context: absolutePath_sourceFolder,
+    entry: './main.js',
+    output: {
+        path: absolutePath_buildFolder,
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.template.html',
+            favicon: './favicon.ico'
+        }),
+        new ProgressBarPlugin()        // [wp|4*]
+    ],
+};
+
+module.exports = config;
+
+
